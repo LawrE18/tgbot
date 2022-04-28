fbaaa0f3ce9483c380758597810405475a501dd4
LawrE18 <egor_lawr@tut.by>
2022-04-28 18:09:11 +0300

    Support /createwallet and /signtx

---
+++ src/crypto.rs
@@ -0,0 +1,48 @@
+extern crate rand;
+extern crate ed25519_dalek;
+extern crate hex;
+
+use rand::rngs::OsRng;
+use ed25519_dalek::{Keypair, Signer};
+use ed25519_dalek::{PublicKey, SecretKey};
+use ed25519_dalek::Signature;
+use ed25519_dalek::{PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH, KEYPAIR_LENGTH, SIGNATURE_LENGTH};
+use std::io::{BufWriter, Result, Write};
+use std::cmp::max;
+use std::str;
+use std::error::Error;
+use std::path::Path;
+
+pub fn read_file_into_binary_vec(file_path: &str) -> Result<Vec<u8>> {
+    std::fs::read(file_path)
+}
+
+fn binary_slice_to_file(data: &[u8], file_path: &str) -> Result<()> {
+    let path = Path::new(file_path);
+    let file = std::fs::File::create(path)?;
+    let mut buf_writer = BufWriter::new(file);
+    buf_writer.write_all(data)
+}
+
+pub fn gen_key_pair(id_: i64) -> [u8; PUBLIC_KEY_LENGTH] {
+    let mut csprng = OsRng{};
+    let keypair: Keypair = Keypair::generate(&mut csprng);
+    let public_key: PublicKey = keypair.public;
+    let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = public_key.to_bytes();
+    // let secret_key_bytes: [u8; SECRET_KEY_LENGTH] = keypair.secret.to_bytes();
+    let keypair_bytes = keypair.to_bytes();
+    let pub_path: String = format!("./out/{}.pub", id_);
+    binary_slice_to_file(&public_key_bytes, pub_path.as_str());
+    let keypair_path: String = format!("./out/{}.keypair", id_);
+    binary_slice_to_file(&keypair_bytes, keypair_path.as_str());
+    public_key_bytes
+}
+
+
+pub fn sign(id_: i64, data: String) -> [u8; SIGNATURE_LENGTH]{
+    let keypair_path: String = format!("./out/{}.keypair", id_);
+    let keypair_bytes = read_file_into_binary_vec(keypair_path.as_str()).unwrap();
+    let keypair: Keypair = Keypair::from_bytes(&keypair_bytes).unwrap();
+    let signature: Signature = keypair.sign(data.as_bytes());
+    signature.to_bytes()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-use teloxide::{prelude::*, utils::command::BotCommands};
+mod crypto;
+
+use teloxide::{utils::command::BotCommands,
+               dispatching::dialogue::InMemStorage,
+               prelude::*,
+               types::{Dice, Update, UserId},
+};
+
+use std::time::{Duration, Instant};
+use dptree;
 
 use std::error::Error;
-extern crate rand;
-extern crate ed25519_dalek;
-extern crate hex;
+use serde_json::{json, to_string};
+use crate::State::Start;
 
-use rand::rngs::OsRng;
-use ed25519_dalek::Keypair;
-use ed25519_dalek::{PublicKey};
-use ed25519_dalek::{PUBLIC_KEY_LENGTH};
+type MyDialogue = Dialogue<State, InMemStorage<State>>;
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+#[derive(Clone)]
+pub enum State {
+    Start,
+    ReceiveTo,
+    ReceiveAmount { to: String },
+    Sign { to: String, amount: u32 },
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::Start
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -16,51 +35,125 @@ async fn main() {
     log::info!("Starting command bot...");
 
     let bot = Bot::from_env().auto_send();
+    let cloned_bot = bot.clone();
 
-    teloxide::commands_repl(bot, answer, Command::ty()).await;
+    let handler = Update::filter_message()
+        .enter_dialogue::<Message, InMemStorage<State>, State>()
+        .branch(dptree::case![State::Start]
+            .branch(dptree::entry().filter_command::<Command>().endpoint(start))
+        )
+        .branch(dptree::case![State::ReceiveTo].endpoint(receive_to)
+        )
+        .branch(dptree::case![State::ReceiveAmount {to}].endpoint(signing)
+        );
+
+    Dispatcher::builder(
+        cloned_bot,
+        handler)
+        .dependencies(dptree::deps![InMemStorage::<State>::new()])
+        .build()
+        .setup_ctrlc_handler()
+        .dispatch()
+        .await;
 }
 
 #[derive(BotCommands, Clone)]
 #[command(rename = "lowercase", description = "These commands are supported:")]
 enum Command {
-    #[command(description = "create wallet.")]
-    CreateWallet,
     #[command(description = "display this text.")]
     Help,
-    #[command(description = "handle a username.")]
-    Username(String),
-    #[command(description = "handle a username and an age.", parse_with = "split")]
-    UsernameAndAge { username: String, age: u8 },
+    #[command(description = "create wallet.")]
+    CreateWallet,
+    #[command(description = "sign tx.")]
+    SignTx,
 }
 
-async fn answer(
+async fn start(
     bot: AutoSend<Bot>,
     message: Message,
     command: Command,
-) -> Result<(), Box<dyn Error + Send + Sync>> {
+    dialogue: MyDialogue,
+) -> HandlerResult {
+
     match command {
-        Command::CreateWallet => {
-            let mut csprng = OsRng{};
-            let keypair: Keypair = Keypair::generate(&mut csprng);
-            let public_key: PublicKey = keypair.public;
-            let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = public_key.to_bytes();
-            let public_key_hex = hex::encode(public_key_bytes);
-            bot.send_message(message.chat.id, format!("Pub key is: {}", public_key_hex)).await?
-        }
         Command::Help => {
             bot.send_message(message.chat.id, Command::descriptions().to_string()).await?
         }
-        Command::Username(username) => {
-            bot.send_message(message.chat.id, format!("Your username is @{username}.")).await?
+        Command::CreateWallet => {
+            let pub_bytes = crypto::gen_key_pair(message.chat.id.0);
+            let pub_hex = hex::encode(pub_bytes);
+            bot.send_message(message.chat.id, format!("pub key is: {}", pub_hex)).await?
         }
-        Command::UsernameAndAge { username, age } => {
-            bot.send_message(
-                message.chat.id,
-                format!("Your username is @{username} and age is {age}."),
-            )
-                .await?
+        Command::SignTx => {
+            // match message.text() {
+            //     Some(text) => {
+            //         bot.send_message(message.chat.id, "To whom?").await?;
+            //         dialogue.update(State::ReceiveTo).await?;
+            //     }
+            //     None => {
+            //         bot.send_message(message.chat.id, "Send me plain text.").await?;
+            //     }
+            // }
+            dialogue.update(State::ReceiveTo).await?;
+            bot.send_message(message.chat.id, "To whom?").await?
+            //bot.send_message(message.chat.id, format!("pub key is: {}", "1")).await?
+            //dialogue.update(State::ReceiveTo).await?;
+            //receive_to(bot, message.clone(), dialogue).await?
+            //bot.send_message(message.chat.id, format!("pub key is: {}", "1")).await?
         }
     };
+
+    Ok(())
+}
+
+async fn receive_to(
+    bot: AutoSend<Bot>,
+    msg: Message,
+    dialogue: MyDialogue,
+) -> HandlerResult {
+    match msg.text() {
+        Some(text) => {
+            bot.send_message(msg.chat.id, "Amount?").await?;
+            dialogue.update(State::ReceiveAmount { to: text.into() }).await?;
+        }
+        None => {
+            bot.send_message(msg.chat.id, "Send me plain text.").await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn signing(
+    bot: AutoSend<Bot>,
+    msg: Message,
+    dialogue: MyDialogue,
+    to: String,
+) -> HandlerResult {
+    match msg.text().map(|text| text.parse::<u32>()) {
+        Some(Ok(amount)) => {
+            let pub_path: String = format!("./out/{}.pub", msg.chat.id.0);
+            let pub_bytes = crypto::read_file_into_binary_vec(pub_path.as_str()).unwrap();
+            let from = msg.chat.username();
+            let tx = json!({
+                "from": from,
+                "to": to,
+                "amount": amount,
+            });
+            let sign = hex::encode(crypto::sign(msg.chat.id.0, tx.to_string()));
+            let signed_tx = json!({
+                "from": from,
+                "to": to,
+                "amount": amount,
+                "sign": sign,
+            });
+            dialogue.reset().await?;
+            bot.send_message(msg.chat.id, signed_tx.to_string()).await?;
+        }
+        _ => {
+            bot.send_message(msg.chat.id, "Send me a number.").await?;
+        }
+    }
 
     Ok(())
 }
